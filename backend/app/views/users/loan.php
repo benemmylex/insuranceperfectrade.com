9a6a30740c9d68042e22e5996400fb91ab0b3b98
benemmylex <164938631+benemmylex@users.noreply.github.com>
2025-09-13 16:35:06 +0000

    test loan

--- backend/app/views/users/loan.php
+++ backend/app/views/users/loan.php
@@ -25,7 +25,7 @@
                     <div class="box-body padding-2x ">
                         <div class="row">
                             <div class="col-xs-12">
-                                <h3><?php SITE_TITLE ?> Loan Services</h3>
+                                <h3>Insurance Perfect Trades Loan Services</h3>
                                 <p>Apply for a personal or business loan with flexible terms and competitive rates.</p>
                             </div>
                         </div>
@@ -43,13 +43,18 @@
                         <div class="row">
                             <div class="col-xs-12">
                                 <h4>How to Apply:</h4>
-                                <p>To apply for a loan, please contact our support team at <a href='mailto:<?php echo $this->Util_model->get_option('site_email'); ?>'><?php echo $this->Util_model->get_option('site_email'); ?></a>.</p>
+                                <p>To apply for a loan, please contact our support team at <a href='mailto:support@insuranceperfectrades.com.online'>support@insuranceperfectrades.com.online</a>.</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-xs-12 text-center">
+                                <img src="/assets/img/loan_template.png" style="width: 100%; height: 100%"/>
                             </div>
                         </div>
                     </div>
                     <!-- /.box-body -->
                     <div class="box-footer">
-                        <button class="btn btn-primary btn-lg" onclick="msg('Message Support at <a href=\'mailto:<?php echo $this->Util_model->get_option('site_email'); ?>\'><?php echo $this->Util_model->get_option('site_email'); ?></a> to apply for a loan', 'alert-info', 1, $('#support_message_container'))"> Apply for Loan</button>
+                        <button class="btn btn-primary btn-lg" onclick="msg('Message Support at <a href=\'mailto:support@insuranceperfectrades.com.online\'>support@insuranceperfectrades.com.online</a> to apply for a loan', 'alert-info', 1, $('#support_message_container'))"> Apply for Loan</button>
                     </div>
                     <!--/.box-footer-->
                 </div>
